21e26ee983e656308325f19334199f682065ac42
Kirill <Akavall@gmail.com>
2015-06-29 02:42:46 -0500

    fixed bug, not making plot works

--- plot_generation.py
+++ plot_generation.py
@@ -12,13 +12,13 @@ def generate_plot_data():
     days_before_break_up_list = []
     prob_down_list = []
     profits_list = []
-    for days_before_break in xrange(1, 251):
+    for days_before_break in xrange(1, 126, 5):
         logging.info("On day : {}".format(days_before_break))
-        for prob_down in np.linspace(0.5, 1, 250):
+        for prob_down in np.linspace(0.5, 1, 25):
             days_before_break_up_list.append(days_before_break)
             prob_down_list.append(prob_down)
-            profits_list.append(calc_profit(250, days_before_break, prob_down))
-    return days_before_break_up, prob_down_list, profits_list
+            profits_list.append(calc_profit(125, days_before_break, prob_down)['profit'])
+    return days_before_break_up_list, prob_down_list, profits_list
 
 def make_3d_scatter_plot(x, y, z, x_label, y_label, z_label):
     fig = plt.figure()
