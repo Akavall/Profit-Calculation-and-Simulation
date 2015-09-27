50732f1fc6bb08770aa19408099d4ab6429fd50a
Kirill <Akavall@gmail.com>
2015-09-27 10:38:05 -0500

    losses are positive now, not tested yet

--- general_simulations.py
+++ general_simulations.py
@@ -16,59 +16,73 @@ def one_pairs_gen(n_periods, conv_rate, break_up_prob, variance):
             # logging.info("Broke up on: {}".format(i))
 
         delta = np.random.uniform(-variance, variance)
-        true_value.append(true_value[-1] * (1 + delta))
+        true_value.append(true_value[-1] + delta)
 
         delta_x = np.random.uniform(-variance, variance)
         delta_y = np.random.uniform(-variance, variance)
 
         if not break_up:                                    
             if x[-1] < true_value[-1]:
-                delta_x += (np.random.uniform(0, variance) * conv_rate)
+                delta_x += ((true_value[-1] - x[-1]) * conv_rate)
             else:
-                delta_x -= (np.random.uniform(0, variance) * conv_rate)
+                delta_x -= ((x[-1] - true_value[-1]) * conv_rate)
 
             if y[-1] < true_value[-1]:
-                delta_y += (np.random.uniform(0, variance) * conv_rate)
+                delta_y += ((true_value[-1] - y[-1]) * conv_rate)
             else:
-                delta_y -= (np.random.uniform(0, variance) * conv_rate)
+                delta_y -= ((y[-1] - true_value[-1]) * conv_rate)
 
-        x.append(x[-1] * (1 + delta_x))
-        y.append(y[-1] * (1 + delta_y))
+        x.append(x[-1] + delta_x)
+        y.append(y[-1] + delta_y)
 
     return {'true_value': true_value, 'x': x, 'y': y}
 
 def calc_profit_sim_wrapper(n_periods, conv_rate, break_up_prob, variance, thresh, n_iterations):
-    total_profits = 0
+    profit = 0
+    profit_from_trading = 0
+    loss = 0
+    trade_info = {}
     for _ in range(n_iterations):
         price_paths = one_pairs_gen(n_periods, conv_rate, break_up_prob, variance)
-        total_profits += calc_profit(price_paths['x'], price_paths['y'], thresh)['profit']
+        trade_result = calc_profit(price_paths['x'], price_paths['y'], thresh)
 
-    return total_profits / n_iterations
+        profit += trade_result['profit']
+        profit_from_trading += trade_result['profit_from_trading']
+        loss += trade_result['loss']
+
+    trade_info = {}
+    trade_info['profit'] = profit / n_iterations
+    trade_info['profit_from_trading'] = profit_from_trading / n_iterations
+    trade_info['loss'] = loss / n_iterations
+
+    return trade_info
     
-
 def calc_profit(x, y, thresh):
     profit_from_trading = 0
     bought_x = False
     bought_y = False 
+    bought_gap = 0
     for ele_x, ele_y in zip(x, y):
         if not bought_x and not bought_y:
             if ele_x - ele_y > thresh:
+                bought_gap = ele_x - ele_y
                 bought_y = True
             elif ele_y - ele_x > thresh:
+                bought_gap = ele_y - ele_x 
                 bought_x = True
         else:
             if bought_x:
                 if ele_x > ele_y:
-                    profit_from_trading += thresh
+                    profit_from_trading += bought_gap
                     bought_x = False
             elif bought_y:
                 if ele_y > ele_x:
-                    profit_from_trading += thresh
+                    profit_from_trading += bought_gap
                     bought_y = False 
     if bought_x:
-        loss = ele_y - ele_x
+        loss = (ele_y - ele_x) - bought_gap
     elif bought_y:
-        loss = ele_x - ele_y
+        loss = (ele_x - ele_y) - bought_gap
     else:
         loss = 0
 
