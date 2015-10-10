68d1b810e149d092aa27be32cac79ca248ed16b3
Akavall <akavall@gmail.com>
2015-10-10 12:24:20 -0500

    the simulation seems right, but the plot is wrong

--- plot_generation.py
+++ plot_generation.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import logging
 
 from calc_profit import calc_profit
+from general_simulations import calc_profit_sim_wrapper
 
 logging.getLogger().setLevel(logging.INFO)
 
@@ -32,10 +33,36 @@ def make_3d_scatter_plot(x, y, z, x_label, y_label, z_label):
 
     plt.show()
 
-if __name__ == "__main__":
-    x, y, z = generate_plot_data()
-    make_3d_scatter_plot(x, y, z, "Days Before Break Up", "Probability to Converge", "Profit")
+def generate_plot_data_general():
+    break_up_prob_list = []
+    conv_prob_list = []
+    profits_list = []
+    for conv_prob in np.linspace(0, 1, 25):
+        logging.info("conv_prob : {}".format(conv_prob))
+        for break_up_prob in np.linspace(0, 1, 25):
+            break_up_prob_list.append(break_up_prob)
+            conv_prob_list.append(break_up_prob)
+            profits_list.append(calc_profit_sim_wrapper(250, conv_prob, break_up_prob, break_up_prob, break_up_prob/100, 1000)['profit'])
+    return break_up_prob_list, conv_prob_list, profits_list
 
+def make_3d_scatter_plot_general(x, y, z, x_label, y_label, z_label):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    ax.scatter(xs=x, ys=y, zs=z, zdir='z', label='ys=0, zdir=z')
+
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+    ax.set_zlabel(z_label)
+
+    plt.show()
+
+if __name__ == "__main__":
+    # x, y, z = generate_plot_data()
+    # make_3d_scatter_plot(x, y, z, "Days Before Break Up", "Probability to Converge", "Profit")
+
+    x, y, z = generate_plot_data_general()
+    make_3d_scatter_plot(x, y, z, "Days Before Break Up", "Probability to Converge", "Profit")
 
 
 
