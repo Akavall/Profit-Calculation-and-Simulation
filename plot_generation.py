fd6ed70bff4ad44ed5ac26d0efdc9e574c943df9
Kirill <Akavall@gmail.com>
2015-10-24 23:10:55 -0500

    working version

--- plot_generation.py
+++ plot_generation.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 import logging
+import json
 
 from calc_profit import calc_profit
 from general_simulations import calc_profit_sim_wrapper
@@ -40,13 +41,14 @@ def generate_plot_data_general():
 
     # coords = [0.0, 0.001, 0.002, 0.003, 0.004, 0.005, 0.006, 0.007, 0.008, 0.009, 0.01, 0.011, 0.012, 0.013, 0.014, 0.015, 0.016, 0.017, 0.018, 0.019, 0.02]
 
-    coords = np.linspace(0, 0.04, 40)
+    conv_prob_cords = np.linspace(0, 1, 40)
+    break_up_coords = np.linspace(0, 0.04, 40)
 
-    for conv_prob in coords:
+    for conv_prob_cords in coords:
         #debug
         #conv_prob = 1
         logging.info("conv_prob : {}".format(conv_prob))
-        for break_up_prob in coords:
+        for break_up_prob in break_up_coords:
             # break_up_prob = 0
             break_up_prob_list.append(break_up_prob)
             conv_prob_list.append(conv_prob)
@@ -74,6 +76,12 @@ if __name__ == "__main__":
     # make_3d_scatter_plot(x, y, z, "Days Before Break Up", "Probability to Converge", "Profit")
 
     x, y, z = generate_plot_data_general()
+    try:
+        with open("/home/kirill/Research/Profit-Calculation-and-Simulation/plot_back_up/plot_1.json", "w") as f:
+            json.dump({"conv": x, "break_up": y, "profits": z})
+    except:
+        logging.INFO("Saving to json failed") 
+
     make_3d_scatter_plot(x, y, z, "Probability to Converge", "Probability of Break Up", "Profit")
 
     # break_up_prob => zero profit because 
